925004c64234a72ac5c642ba54256e183b4369f3
C. E. Guergolet <82114318+CEGS9@users.noreply.github.com>
2022-03-05 21:14:36 -0300

    Add files via upload

---
+++ JavaScript/index_primavera.js
@@ -0,0 +1,15 @@
+var countDownDate = new Date("September 22, 2022 22:04:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+document.getElementById("dday").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+if (distance < 0) {
+  clearInterval(x);
+  document.getElementById("dday").innerHTML = "Ops! Já estamos na primavera";
+  }
+}, 1000);
---
+++ JavaScript/index_verao.js
@@ -0,0 +1,15 @@
+var countDownDate = new Date("December 21, 2021 12:59:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+document.getElementById("dday").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+if (distance < 0) {
+  clearInterval(x);
+  document.getElementById("dday").innerHTML = "Ops! Já estamos no verão";
+  }
+}, 1000);
